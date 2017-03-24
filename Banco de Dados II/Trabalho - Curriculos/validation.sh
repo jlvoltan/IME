4c67b27391603915c8ac6bb7785e85b84f245295
Pedro Igor <pedroigorao@hotmail.com>
2017-03-24 05:09:55 -0300

    finishing

--- Banco de Dados II/Trabalho - Curriculos/validation.sh
+++ Banco de Dados II/Trabalho - Curriculos/validation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "5 curriculos base para schema"
-xmllint --noout --schema Parte_2/schema.xsd Parte_2/5\ validados/*
+xmllint --noout --schema Parte_2/schema.xsd Parte_2/5_validados/*
 
 echo ""
 
 echo "20 curriculos ajustados pelo XQuery"
-xmllint --noout --schema Parte_2/schema.xsd Parte_3/Novos\ 20\ curriculos/*
+xmllint --noout --schema Parte_2/schema.xsd Parte_3/Novos_20_curriculos/*
